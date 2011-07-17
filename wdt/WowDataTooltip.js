c1cda5ba1b0f6893bf7376dbd1f3cb4009f528bc
Martin Gelder <darkspotinthecorner@gmail.com>
2011-07-17 04:46:54 +0000

    Shift Funktionalität eingebaut

--- wdt/WowDataTooltip.js
+++ wdt/WowDataTooltip.js
@@ -25,11 +25,370 @@ if(!window.yepnope) {
 }
 
 var WowDataTooltip = {
+	
+	'activeTooltip': false,
+	
+	mustache: function() {
+		
+		var Renderer = function() {};
+		
+		Renderer.prototype = {
+			otag: "{{",
+			ctag: "}}",
+			pragmas: {},
+			buffer: [],
+			pragmas_implemented: {
+				"IMPLICIT-ITERATOR": true
+			},
+			context: {},
+		    render: function(template, context, partials, in_recursion) {
+				
+				// reset buffer & set context
+				if(!in_recursion) {
+					this.context = context;
+					this.buffer = []; // TODO: make this non-lazy
+				}
+				
+				// fail fast
+				if(!this.includes("", template)) {
+					if(in_recursion) {
+						return template;
+					} else {
+						this.send(template);
+						return;
+					}
+				}
+				
+				template = this.render_pragmas(template);
+				var html = this.render_section(template, context, partials);
+				if(in_recursion) {
+					return this.render_tags(html, context, partials, in_recursion);
+				}
+				
+				this.render_tags(html, context, partials, in_recursion);
+				
+			},
+			
+			/*
+				Sends parsed lines
+			*/
+			send: function(line) {
+				
+				if(line != "") {
+					this.buffer.push(line);
+				}
+				
+			},
+			
+			/*
+				Looks for %PRAGMAS
+			*/
+			render_pragmas: function(template) {
+				
+				// no pragmas
+				if(!this.includes("%", template)) {
+					return template;
+				}
+				
+				var that = this;
+				var regex = new RegExp(this.otag + "%([\\w-]+) ?([\\w]+=[\\w]+)?" + this.ctag, "g");
+				return template.replace(regex, function(match, pragma, options) {
+					if(!that.pragmas_implemented[pragma]) {
+						throw({message: 
+							"This implementation of mustache doesn't understand the '" +
+							pragma + "' pragma"});
+					}
+					that.pragmas[pragma] = {};
+					if(options) {
+						var opts = options.split("=");
+						that.pragmas[pragma][opts[0]] = opts[1];
+					}
+	        		return "";
+					// ignore unknown pragmas silently
+				});
+				
+			},
+			
+			/*
+				Tries to find a partial in the curent scope and render it
+			*/
+			render_partial: function(name, context, partials) {
+				
+				name = this.trim(name);
+				if(!partials || partials[name] === undefined) {
+					throw({message: "unknown_partial '" + name + "'"});
+				}
+				if(typeof(context[name]) != "object") {
+					return this.render(partials[name], context, partials, true);
+				}
+				return this.render(partials[name], context[name], partials, true);
+			},
+			
+			/*
+				Renders inverted (^) and normal (#) sections
+			*/
+			render_section: function(template, context, partials) {
+				
+				if(!this.includes("#", template) && !this.includes("^", template)) {
+					return template;
+				}
+				
+				var that = this;
+				// CSW - Added "+?" so it finds the tighest bound, not the widest
+				var regex = new RegExp(this.otag + "(\\^|\\#)\\s*(.+)\\s*" + this.ctag + "\n*([\\s\\S]+?)" + this.otag + "\\/\\s*\\2\\s*" + this.ctag + "\\s*", "mg");
+				
+				// for each {{#foo}}{{/foo}} section do...
+				return template.replace(regex, function(match, type, name, content) {
+					
+					var value = that.find(name, context);
+					if(type == "^") { // inverted section
+						if(!value || that.is_array(value) && value.length === 0) {
+							// false or empty list, render it
+							return that.render(content, context, partials, true);
+						} else {
+							return "";
+						}
+					} else if(type == "#") { // normal section
+						if(that.is_array(value)) { // Enumerable, Let's loop!
+							return that.map(value, function(row) {
+								return that.render(content, that.create_context(row), partials, true);
+							}).join("");
+						} else if(that.is_object(value)) { // Object, Use it as subcontext!
+							return that.render(content, that.create_context(value), partials, true);
+						} else if(typeof value === "function") {
+							// higher order section
+							return value.call(context, content, function(text) {
+								return that.render(text, context, partials, true);
+							});
+						} else if(value) { // boolean section
+							return that.render(content, context, partials, true);
+						} else {
+							return "";
+						}
+					}
+				});
+			},
+			
+			/*
+				Replace {{foo}} and friends with values from our view
+			*/
+			render_tags: function(template, context, partials, in_recursion) {
+				
+				// tit for tat
+				var that = this;
+				
+				var new_regex = function() {
+					
+					return new RegExp(that.otag + "(=|!|>|\\{|%)?([^\\/#\\^]+?)\\1?" + that.ctag + "+", "g");
+					
+				};
+				
+				var regex = new_regex();
+				var tag_replace_callback = function(match, operator, name) {
+					
+					switch(operator) {
+						case "!": // ignore comments
+							return "";
+						case "=": // set new delimiters, rebuild the replace regexp
+							that.set_delimiters(name);
+							regex = new_regex();
+							return "";
+						case ">": // render partial
+							return that.render_partial(name, context, partials);
+						case "{": // the triple mustache is unescaped
+							return that.find(name, context);
+						default: // escape the value
+							return that.escape(that.find(name, context));
+					}
+					
+	      		};
+	      		var lines = template.split("\n");
+				for(var i = 0; i < lines.length; i++) {
+					lines[i] = lines[i].replace(regex, tag_replace_callback, this);
+					if(!in_recursion) {
+						this.send(lines[i]);
+					}
+				}
+				if(in_recursion) {
+					return lines.join("\n");
+				}
+				
+			},
+			
+			set_delimiters: function(delimiters) {
+				
+				var dels = delimiters.split(" ");
+				this.otag = this.escape_regex(dels[0]);
+				this.ctag = this.escape_regex(dels[1]);
+				
+			},
+			
+			escape_regex: function(text) {
+				
+				// thank you Simon Willison
+				if(!arguments.callee.sRE) {
+					var specials = ['/', '.', '*', '+', '?', '|', '(', ')', '[', ']', '{', '}', '\\'];
+					arguments.callee.sRE = new RegExp('(\\' + specials.join('|\\') + ')', 'g');
+	      		}
+				
+				return text.replace(arguments.callee.sRE, '\\$1');
+				
+			},
+			
+			/*
+				find `name` in current `context`. That is find me a value
+				from the view object
+			*/
+			find: function(name, context) {
+				
+				name = this.trim(name);
+				
+				// Checks whether a value is thruthy or false or 0
+				function is_kinda_truthy(bool) {
+					
+					return bool === false || bool === 0 || bool;
+					
+				}
+				
+				var value;
+				if(is_kinda_truthy(context[name])) {
+					value = context[name];
+				} else if(is_kinda_truthy(this.context[name])) {
+					value = this.context[name];
+				}
+				
+				if(typeof value === "function") {
+					return value.apply(context);
+				}
+				if(value !== undefined) {
+					return value;
+				}
+				// silently ignore unkown variables
+				return "";
+				
+			},
+			
+			// Utility methods
+			
+			/* includes tag */
+			includes: function(needle, haystack) {
+				
+				return haystack.indexOf(this.otag + needle) != -1;
+				
+			},
+			
+			/*
+				Does away with nasty characters
+			*/
+			escape: function(s) {
+				
+				s = String(s === null ? "" : s);
+				return s.replace(/&(?!\w+;)|["'<>\\]/g, function(s) {
+					switch(s) {
+						case "&": return "&amp;";
+						case "\\": return "\\\\";
+						case '"': return '&quot;';
+						case "'": return '&#39;';
+						case "<": return "&lt;";
+						case ">": return "&gt;";
+						default: return s;
+					}
+				});
+			},
+			
+			// by @langalex, support for arrays of strings
+			create_context: function(_context) {
+				
+				if(this.is_object(_context)) {
+					return _context;
+				} else {
+					var iterator = ".";
+					if(this.pragmas["IMPLICIT-ITERATOR"]) {
+						iterator = this.pragmas["IMPLICIT-ITERATOR"].iterator;
+					}
+					var ctx = {};
+					ctx[iterator] = _context;
+					return ctx;
+				}
+				
+			},
+			
+			is_object: function(a) {
+				
+				return a && typeof a == "object";
+				
+			},
+			
+			is_array: function(a) {
+				
+				return Object.prototype.toString.call(a) === '[object Array]';
+				
+			},
+			
+			/*
+				Gets rid of leading and trailing whitespace
+			*/
+			trim: function(s) {
+				
+				return s.replace(/^\s*|\s*$/g, "");
+				
+			},
+			
+			/*
+				Why, why, why? Because IE. Cry, cry cry.
+			*/
+			map: function(array, fn) {
+				
+				if (typeof array.map == "function") {
+					return array.map(fn);
+				} else {
+					var r = [];
+					var l = array.length;
+					for(var i = 0; i < l; i++) {
+						r.push(fn(array[i]));
+					}
+					return r;
+				}
+			}
+		};
+		
+		return({
+			name: "mustache.js",
+			version: "0.3.1-dev",
+			
+			/*
+				Turns a template and view into HTML
+			*/
+			to_html: function(template, view, partials, send_fun) {
+				
+				var renderer = new Renderer();
+				if(send_fun) {
+					renderer.send = send_fun;
+				}
+				renderer.render(template, view, partials);
+				if(!send_fun) {
+					return renderer.buffer.join("\n");
+				}
+			}
+		});
+	}(),
+	
 	addTip: function(element, tipcontent) {
 		jQuery(element).qtip({
 			overwrite: false, // Make sure another tooltip can't overwrite this one without it being explicitly destroyed
 			show: {
 				ready: true // Needed to make it show on first mouseover event
+			},
+			events: {
+				render: function(event, api) {
+					var tooltip = api.elements.tooltip;
+					tooltip.bind('tooltipshow', function(event, api) {
+						WowDataTooltip.activeTooltip = api.id;
+					});
+					tooltip.bind('tooltiphide', function(event, api) {
+						WowDataTooltip.activeTooltip = false;
+					});
+				},
 			},
 			content: {
 				text: tipcontent
@@ -41,10 +400,11 @@ var WowDataTooltip = {
 			},
 			hide: 'mouseout',
 			style: {
-				classes: 'ui-tooltip-wdt-dark'
+				classes: 'wdt-tooltip ui-tooltip-wdt-dark'
 			}
 		});
 	},
+	
 	getLocale: function(lang) {
 		if('object' === typeof(WowDataTooltip['i18n'][lang])) {
 			var loc = WowDataTooltip['i18n'][lang];
@@ -53,6 +413,7 @@ var WowDataTooltip = {
 		}
 		return loc;
 	},
+	
 	getTemplate: function(template, type) {
 		if('undefined' == typeof(WowDataTooltip['templates'][template])) {
 			return 'Template "'+template+'" not found!';
@@ -62,46 +423,72 @@ var WowDataTooltip = {
 		}
 		return WowDataTooltip['templates'][template][type];
 	},
-	buildCharacterTooltip: function(region, loc, href, data) {
-		var content   = '';
-		var avataruri = WowDataTooltip['patterns']['character']['avatar'];
-		avataruri     = avataruri.replace(/\{region\}/g,    region);
-		avataruri     = avataruri.replace(/\{thumbnail\}/g, data['thumbnail']);
-		avataruri     = avataruri.replace(/\{race\}/g,      data['race']);
-		avataruri     = avataruri.replace(/\{gender\}/g,    data['gender']);
-		if('object' === typeof(loc['class:'+data['class']])) {
-			var classname = loc['class:'+data['class']]['gender:'+data['gender']];
-		} else {
-			var classname = loc['class:'+data['class']];
-		}
-		if('object' === typeof(loc['race:'+data['race']])) {
-			var racename = loc['race:'+data['race']]['gender:'+data['gender']];
-		} else {
-			var racename = loc['race:'+data['race']];
-		}
-		var lrc = loc['format:level+class+race'];
-		lrc     = lrc.replace(/\{level\}/g, data['level']);
-		lrc     = lrc.replace(/\{race\}/g,  racename);
-		lrc     = lrc.replace(/\{class\}/g, classname);
+	
+	localize: function(repository, keys, replacements) {
+		var temp   = repository;
+		var result = '';
 		
-		content = WowDataTooltip.getTemplate('default', 'character');
-		content = content.replace(/\{avataruri\}/g,         avataruri);
-		content = content.replace(/\{classid\}/g,           data['class']);
-		content = content.replace(/\{name\}/g,              data['name']);
-		content = content.replace(/\{realm\}/g,             data['realm']);
-		content = content.replace(/\{achievementpoints\}/g, data['achievementPoints']);
-		content = content.replace(/\{lrc\}/g,               lrc);
-		if(data['guild']) {
-			content = content.replace(/\{guild\}/g, ('&lt;' + data['guild']['name'] + '&gt;'));
-		} else {
-			content = content.replace(/\{guild\}/g, '');
+		if('string' === typeof(keys)) {
+			keys = [keys]; 
 		}
+		
+		for (var i = 0; i < keys.length; i++) {
+			if('undefined' === typeof(temp[keys[i]])) {
+				result = '';
+			} else {
+				if('object' === typeof(temp[keys[i]])) {
+					temp = temp[keys[i]];
+				} else {
+					result = temp[keys[i]];
+				}
+			}
+		}
+		
+		if(('' !== result) && ('object' === typeof(replacements))) {
+			for (i in replacements) {
+				console.log({'i':i, 'replacements[i]': replacements[i]});
+				var regexp = new RegExp('\{'+i+'\}', 'gi');
+				result = result.replace(regexp, replacements[i]);
+			}
+		}
+		
+		return result;
+	},
+	
+	buildCharacterTooltip: function(region, loc, apicall, data) {
+		var content   = '';
+		
+		var classname = this.localize(loc, [('class:'+data['class']), ('gender:'+data['gender'])]);
+		var racename  = this.localize(loc, [('race:'+data['class']),  ('gender:'+data['gender'])]);
+		var lrc       = this.localize(loc, 'format:level+class+race', {'level': data['level'], 'race': racename, 'class': classname});
+		
+		var host = apicall.replace(/\/api\/wow\/.+$/i, '');
+		
+		var tvars = {
+			'path.host': host,
+			'region': region,
+			'thumbnail': data['thumbnail'],
+			'talents': data['talents'],
+			'name': data['name'],
+			'realm': data['realm'],
+			'guild': data['guild'],
+			'classid': data['class'],
+			'raceid': data['race'],
+			'genderid': data['gender'],
+			'achievementpoints': data['achievementPoints'],
+			'lrc': lrc
+		};
+		
+		var content = WowDataTooltip.mustache.to_html(WowDataTooltip.getTemplate('default', 'character'), tvars);
+		
 		return content;
 	},
+	
 	addToCache: function(type, apicall, content) {
 		WowDataTooltip['cache'][type][apicall] = content;
 		return true;
 	},
+	
 	getCache: function(type, apicall) {
 		if('undefined' == typeof(WowDataTooltip['cache'][type])) {
 			WowDataTooltip['cache'][type] = {};
@@ -112,28 +499,47 @@ var WowDataTooltip = {
 		}
 		return false;
 	},
-	'resources': {
-		'jquery'      : 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js',
-		'qtip2.plugin': 'qtip2/jquery.qtip.min.js',
-		'qtip2.css'   : 'qtip2/jquery.qtip.min.css',
-		'wdt.css'     : 'wdt/WowDataTooltip.css'
+	
+	repositionActiveTooltips: function() {
+		// Find all active wdt tooltips and run .reposition on them
+		if(WowDataTooltip.activeTooltip !== false) {
+			jQuery('#ui-tooltip-'+WowDataTooltip.activeTooltip).qtip('reposition');
+		}
 	},
+	
+	'resources': {
+		'jquery'         : 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js',
+		'qtip2.plugin'   : 'qtip2/jquery.qtip.min.js',
+		'qtip2.css'      : 'qtip2/jquery.qtip.min.css',
+		'wdt.css'        : 'wdt/WowDataTooltip.css'
+	},
+	
 	'templates': {
 		'default': {
 			'character': (
-				'<div class="tooltip_character template-default">'                 + "\n" +
-			    '    <img class="avatar" src="{avataruri}" />'                     + "\n" +
-			    '    <div class="data">'                                           + "\n" +
-			    '        <div class="name cclass-{classid}">{name}</div>'          + "\n" +
-			    '        <div class="level class race">{lrc}</div>'                + "\n" +
-				'        <div class="guild">{guild}</div>'                         + "\n" +
-				'        <div class="realm">{realm}</div>'                         + "\n" +
-				'        <div class="achievementpoints">{achievementpoints}</div>' + "\n" +
-			    '    </div>'                                                       + "\n" +
+				'<div class="tooltip-character template-default">' +
+			    	'<img class="thumbnail" src="{{path.host}}/static-render/{{region}}/{{thumbnail}}?alt=/wow/static/images/2d/avatar/{{raceid}}-{{genderid}}.jpg" />' +
+			    	'<div class="data">' +
+			    		'<div class="name cclass-{{classid}}">{{name}}</div>' +
+			    		'<div class="level class race">{{lrc}}</div>' +
+						'{{#talents}}' +
+			    		'<div class="talentspec wdt-show-only-extended{{#selected}} active{{/selected}}">' +
+							'<img class="icon-talentspec" src="http://{{region}}.media.blizzard.com/wow/icons/18/{{icon}}.jpg"/> {{name}}' +
+						'</div>' +
+						'{{/talents}}' +
+						'{{#guild}}' +
+						'<div class="guild">&lt;{{name}}&gt;{{#level}} ({{level}}){{/level}}</div>' +
+						'{{/guild}}' +
+						'<div class="realm">{{realm}}</div>' +
+						'<div class="achievementpoints">{{achievementpoints}}</div>' +
+						'<div class="info-meta wdt-show-only-simple">Hold [Shift] for extended mode!</div>' +
+						'<div class="info-meta wdt-show-only-extended">Relese [Shift] for simple mode!</div>' +
+			    	'</div>' +
 			    '</div>'
 			)
 		}
 	},
+	
 	'i18n': {
 		'en': {
 			'format:level+class+race': '{level} {race} {class}',
@@ -311,13 +717,15 @@ var WowDataTooltip = {
 			'race:22' : 'Worgen',
 		}
 	},
+	
 	'patterns': {
 		'character': {
 			'regex' : /http:\/\/(eu|us|kr|tw).battle.net\/wow\/(en|de|fr|es|ru|ko|zh)\/character\/([^\/]+)\/([^\/]+)\/.*/,
-			'api'   : 'http://$1.battle.net/api/wow/character/$3/$4?fields=guild,talents',
+			'api'   : 'http://$1.battle.net/api/wow/character/$3/$4?fields=guild,talents,items',
 			'avatar': 'http://{region}.battle.net/static-render/{region}/{thumbnail}?alt=/wow/static/images/2d/avatar/{race}-{gender}.jpg'
 		}
 	},
+	
 	'cache': {
 		'character': {}
 	}
@@ -339,6 +747,28 @@ yepnope([{
 					
 					// --- Loading complete ------------------------------------
 					
+					jQuery('body').bind('keydown', function(event) {
+						
+						// Try to make shift key change the tooltip style
+						if(event.keyCode == 16) {
+							// WowDataTooltip.extendedActive = true;
+							jQuery('body').addClass('wdt-show-extended');
+							WowDataTooltip.repositionActiveTooltips();
+						}
+						
+					});
+					
+					jQuery('body').bind('keyup', function(event) {
+						
+						// Try to make shift key change the tooltip style
+						if(event.keyCode == 16) {
+							// WowDataTooltip.extendedActive = false;
+							jQuery('body').removeClass('wdt-show-extended');
+							WowDataTooltip.repositionActiveTooltips();
+						}
+						
+					});
+					
 					jQuery('body').delegate('a[href]', 'mouseover', function() {
 						
 						// In a tooltip is already attached, don't re-render it, just show it
@@ -348,7 +778,7 @@ yepnope([{
 							
 						} else {
 							
-							var content = 'Loading...';
+							var content = '';
 							var href    = new String(jQuery(this).attr('href'));
 							var apicall = null;
 							var params  = null;
@@ -356,7 +786,7 @@ yepnope([{
 							var result  = href.match(WowDataTooltip['patterns']['character']['regex']);
 							if(result) {
 								
-								WowDataTooltip.addTip(this, content);
+								WowDataTooltip.addTip(this, 'Loading character...');
 								
 								params = {
 									'region'   : result[1],
@@ -366,7 +796,6 @@ yepnope([{
 								};
 								
 								apicall = href.replace(WowDataTooltip['patterns']['character']['regex'], WowDataTooltip['patterns']['character']['api']);
-								// console.log('API Calling: '+apicall);
 								
 								content = WowDataTooltip.getCache('character', apicall);
 								
@@ -386,7 +815,7 @@ yepnope([{
 										success: function(data) {
 											
 											var loc     = WowDataTooltip.getLocale(params['lang']);
-											var content = WowDataTooltip.buildCharacterTooltip(params['region'], loc, href, data);
+											var content = WowDataTooltip.buildCharacterTooltip(params['region'], loc, apicall, data);
 											
 											// WowDataTooltip.addTip(this, content);
 											jQuery(this).qtip('api').set('content.text', content);
@@ -397,6 +826,11 @@ yepnope([{
 									});
 									
 								}
+								
+							}
+							
+							result = false;
+							if(result) {
 								
 							}
 							
